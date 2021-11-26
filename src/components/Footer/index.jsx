e6f514df60c090a4b928936b3e1646f1674d2d5c
Laura Warnava <laurawwarnava@gmail.com>
2021-11-25 21:34:08 -0300

    Finalização do componente Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './style.scss';
+import iconTop from '../../images/icon-top.png';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer__section footer__section--first">
+                <div className="footer__container">
+                    <p>
+                        <strong>Observação:</strong> este site é uma amostra
+                        Fde desenvolvimento tecnológico de iniciativa independente,
+                        as informações aqui apresentadas podem estar eventualmente desatualizadas,
+                        por isso recomendamos sempre conferir as informações diretamente com o estabelecimento.
+                        A exibição dos locais, nesta página não caracteria recomendação de uso.
+                    </p>
+                </div>
+            </div>
+            <div className="footer__section footer__section--second">
+                <div className="footer__container">
+                    <p>
+                        <strong>Projeto Elaborado durante a RDW <a href="https://ebaconline.com.br" target="blank"> E.B.A.C
+                        </a> 2021 </strong>
+                        <small>
+                        Projetado por <a href="https://github.com/cavalcantemmarcelo" target="blank"> Marcelo Cavalcante </a> 
+                        e <a href="https://github.com/mja-maia" target="blank"> Miguel Maia </a>,
+                        desenvolvido por <a href="https://github.com/laurawarnava" target="blank"> Laura Warnava</a>.</small>
+                    </p>
+                    <a href="#top" className="button">
+                        <img src={iconTop} alt="Voltar ao topo" />
+                    </a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/global.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
 import CuratorItem from './components/Curator';
+import Footer from './components/Footer';
 
 const CONFIG = {
   city: "São Paulo",
@@ -18,6 +19,7 @@ ReactDOM.render(
       {/* Espaço para inserir a lista de lugares*/}
       <CuratorItem />
     </div>
+      <Footer />
   </div>
   ,
   document.getElementById('root')
