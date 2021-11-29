4528531a5f972c7de70eebbb78bd6fcd74518da4
Laura Warnava <laurawwarnava@gmail.com>
2021-11-29 20:31:23 -0300

    Aplicação de responsividade

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -20,6 +20,9 @@ const Home = props => {
         <main id="main-content" className="home__container">
             <div className="home__col">
                 <h1 className="home__title">{props.city} para todos</h1>
+                <div className="home__image home__image--destack hide-desktop">
+                    <img src={imageDestack} alt='Imagem destaque cadeirante' />
+                </div>
                 <p className="home__text">
                     Nossa missão é facilitar o encontro entre lugares inclusivos e pessoas que buscam alternativas culturais para sair de casa, com a mobilidade que todo cidadão precisa.
                 </p>
@@ -39,7 +42,7 @@ const Home = props => {
                 </div>
             </div>
             <div className="home__col">
-                <div className="home__image home__image--destack">
+                <div className="home__image home__image--destack hide-mobile">
                     <img src={imageDestack} alt='Imagem destaque cadeirante' />
                 </div>
             </div>
